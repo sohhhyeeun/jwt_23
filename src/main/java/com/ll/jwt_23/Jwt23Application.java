78cb3572c9db0ce56e65d432410745b5e0ef4d9a
sohhhyeeun <syeankm12076@gmail.com>
2023-05-16 15:03:49 +0900

    세팅

---
+++ src/main/java/com/ll/jwt_23/Jwt23Application.java
@@ -0,0 +1,13 @@
+package com.ll.jwt_23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwt23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jwt23Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/jwt_23/Jwt23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.jwt_23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwt23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
